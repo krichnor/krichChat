b4d4bba68292d9c182afff178babba20da2638b2
krichnor <krich.norakhun@gmail.com>
2024-08-06 00:21:06 +0700

    cmt

--- krichChat.py
+++ krichChat.py
@@ -9,3 +9,18 @@ os.environ["ANTHROPIC_API_KEY"] = os.getenv('ANTHROPIC_API_KEY')
 os.environ["TAVILY_API_KEY"]=os.getenv('TAVILY_API_KEY')
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv('LANGCHAIN_API_KEY')
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.vectorstores import Chroma
+import json
+import myPackage.GetRaw 
+import myPackage.PartDataFrame
+import myPackage.APIToDF
